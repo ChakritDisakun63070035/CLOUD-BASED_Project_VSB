c533ebedc144ca027de3cc16807c982a438a777f
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-04-11 04:34:00 +0700

    update ja pai sleep kannn

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,53 @@
-const express = require("express");
-const pool = require("../config");
+const express = require("express")
+const pool = require("../config")
+const path = require("path")
+const bodyParser = require("body-parser")
 
-router = express.Router();
+router = express.Router()
 
 router.get("/", async function (req, res, next) {
-    try {
-        const [rows, fields] = await pool.query(
-          'SELECT * FROM course'
-        );
-        return res.render("index", { courses: JSON.stringify(rows) });
-      } catch (err) {
-        return next(err)
-      }
-});
+  try {
+    const [rows, fields] = await pool.query("SELECT * FROM course")
+    return res.render("index", { courses: JSON.stringify(rows) })
+  } catch (err) {
+    return next(err)
+  }
+})
 
-exports.router = router;
+router.get("/sign-up", async function (req, res, next) {
+  res.render("user/sign-up")
+})
+
+router.post("/sign-up", async function (req, res, next) {
+  const fname = req.body.fname
+  const lname = req.body.lname
+  const email = req.body.email
+  const password = req.body.password
+  const dob = req.body.dob
+  const gender = req.body.gender
+
+  const conn = await pool.getConnection()
+  await conn.beginTransaction()
+
+  try {
+    const [rows, fields] = await conn.query("INSERT INTO user (user_fname, user_lname, email, password, dateofbirth, gender) VALUES(?, ?, ?, ?, ?, ?)", [
+      fname,
+      lname,
+      email,
+      password,
+      dob,
+      gender
+    ])
+
+    await conn.commit()
+    res.redirect("/")
+  } catch (err) {
+    await conn.rollback()
+    next(err)
+  } finally {
+    console.log("finally")
+    await conn.release()
+  }
+})
+
+exports.router = router
