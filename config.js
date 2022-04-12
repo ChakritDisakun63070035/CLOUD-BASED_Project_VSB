20b49d9427e2d160a7ba55bd058ab8c1de879d49
YamonpornChertbamrung <63070140@kmitl.ac.th>
2022-04-13 00:22:06 +0700

    แก้configเฉยๆ

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '123456',
-  database: 'VSB',
+  password: 'modji2002',
+  database: 'db_project',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
