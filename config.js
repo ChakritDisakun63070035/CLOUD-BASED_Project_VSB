43d7bb33a26cfa21bd38e0aafce7a5d105b99f41
YamonpornChertbamrung <63070140@kmitl.ac.th>
2022-04-15 03:52:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
   password: 'modji2002',
-  database: 'db_project',
+  database: 'project-web',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
