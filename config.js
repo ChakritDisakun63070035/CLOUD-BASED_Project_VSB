4519f58f33695f5ac535be2e7bd09ea72ba452c2
ChakritDisakun63070035 <zix.zak@hotmail.com>
2022-04-16 22:06:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '931755',
-  database: 'vsb_4',
+  password: '123456',
+  database: 'VSB2',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
