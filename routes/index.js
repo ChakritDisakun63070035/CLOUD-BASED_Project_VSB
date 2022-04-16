a88fcce8ae92f56ee8bda889e42bc9d216981ffb
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-04-17 03:47:39 +0700

    .

--- routes/index.js
+++ routes/index.js
@@ -31,8 +31,9 @@ router.get("/allcourse/:id", async function (req, res, next) {
   const conn = await pool.getConnection()
   await conn.beginTransaction()
   try {
-    const [rows, fields] = await conn.query("SELECT * FROM course")
-    const [rows1, fields1] = await conn.query("SELECT * FROM user WHERE user_id=?", [req.params.id])
+    const [rows, fields] = await conn.query("SELECT * FROM `course`")
+    const [rows1, fields1] = await conn.query("SELECT * FROM `user` WHERE user_id=?", [req.params.id])
+    // const [rows2, fields2] = await conn.query("SELECT * FROM `order` WHERE user_id=?", [req.params.id])
     return res.render("allcourse", { courses: JSON.stringify(rows), users: JSON.stringify(rows1) })
   } catch (err) {
     console.log(err)
@@ -74,13 +75,17 @@ router.get("/allcourse/not-sign-in/:id", async function (req, res, next) {
 })
 
 // my cart
-router.get("/mycart/:id", async function (req, res, next) {
+router.get("/mycart/:id/", async function (req, res, next) {
   try {
-    const [rows1, fields1] = await pool.query("SELECT * FROM `order` WHERE user_id=?", [req.params.id])
-    let order_id = rows1[0].order_id
-    const [rows2, fields2] = await pool.query("SELECT * FROM `order_item` JOIN `course` USING(course_id) WHERE order_id=?", [order_id])
-    const [rows3, fields3] = await pool.query("SELECT * FROM `user` WHERE user_id=?", [req.params.id])
-    return res.render("user/cart", { items: JSON.stringify(rows2), users: JSON.stringify(rows3), carts: JSON.stringify(rows1) })
+    const [rows1, fields1] = await pool.query("SELECT * FROM `order` WHERE user_id=? AND order_status=?", [req.params.id, "pending"])
+    if (rows1.length > 0) {
+      let order_id = rows1[0].order_id
+      const [rows2, fields2] = await pool.query("SELECT * FROM `order_item` JOIN `course` USING(course_id) WHERE order_id=?", [order_id])
+      const [rows3, fields3] = await pool.query("SELECT * FROM `user` WHERE user_id=?", [req.params.id])
+      return res.render("user/cart", { items: JSON.stringify(rows2), users: JSON.stringify(rows3), carts: JSON.stringify(rows1) })
+    } else{
+      res.send("nothing in your cart.")
+    }
   } catch (err) {
     return next(err)
   }
@@ -92,26 +97,27 @@ router.get("/course/:course_id/create/cart/:user_id/:price", async function (req
   await conn.beginTransaction()
   const admin = Math.floor(Math.random() * (7 - 1)) + 1
   try {
-    const [order, fields1] = await conn.query("SELECT * FROM `order` WHERE user_id=?", [req.params.user_id])
+    const [order, fields1] = await conn.query("SELECT * FROM `order` WHERE user_id=? AND order_status=?", [req.params.user_id, "pending"])
     if (order.length > 0) {
       let order_status = order[0].order_status
       let order_id = order[0].order_id
-      if (order_status !== "complete") {
-        const [additem, fields3] = await conn.query("INSERT INTO order_item (item_price, course_id, order_id) VALUES(?, ?, ?)", [req.params.price, req.params.course_id, order_id])
-        const [itemprice, fields4] = await conn.query("SELECT SUM(item_price) AS total FROM `order_item` WHERE order_id=?", [order_id])
-        let total = itemprice[0].total
-        const [orderprice, fields5] = await conn.query("UPDATE `order` SET price_total=? WHERE order_id=?", [total, order_id])
-      }
+      const [additem1, fields1] = await conn.query("INSERT INTO order_item (item_price, course_id, order_id) VALUES(?, ?, ?)", [req.params.price, req.params.course_id, order_id])
+      const [additem2, fields2] = await conn.query("INSERT INTO my_course (course_id, order_id) VALUES(?, ?)", [req.params.course_id, order_id])
+      const [itemprice1, fields4] = await conn.query("SELECT SUM(item_price) AS total FROM `order_item` WHERE order_id=?", [order_id])
+      let total1 = itemprice1[0].total
+      const [orderprice1, fields5] = await conn.query("UPDATE `order` SET price_total=? WHERE order_id=?", [total1, order_id])
     } else {
       const [createcart, fields2] = await conn.query("INSERT INTO `order` (order_date, user_id, admin_id) VALUES(CURDATE(), ?, ?)", [req.params.user_id, admin])
-      const [additem, fields3] = await conn.query("INSERT INTO order_item (item_price, course_id, order_id) VALUES(?, ?, ?)", [
+      const [additem2, fields6] = await conn.query("INSERT INTO `order_item` (item_price, course_id, order_id) VALUES(?, ?, ?)", [
         req.params.price,
         req.params.course_id,
         createcart.insertId,
       ])
-      const [itemprice, fields4] = await conn.query("SELECT SUM(item_price) AS total FROM `order_item` WHERE order_id=?", [createcart.insertId])
-      let total = itemprice[0].total
-      const [orderprice, fields5] = await conn.query("UPDATE `order` SET price_total=? WHERE order_id=?", [total, createcart.insertId])
+      const [additem3, fields3] = await conn.query("INSERT INTO my_course (course_id, order_id) VALUES(?, ?)", [req.params.course_id, createcart.insertId])
+      const [itemprice2, fields7] = await conn.query("SELECT SUM(item_price) AS total FROM `order_item` WHERE order_id=?", [createcart.insertId])
+      let total2 = itemprice2[0].total
+
+      const [orderprice2, fields8] = await conn.query("UPDATE `order` SET price_total=? WHERE order_id=?", [total2, createcart.insertId])
     }
     await conn.commit()
     res.redirect("/course/" + req.params.course_id + "/" + req.params.user_id)
@@ -145,12 +151,61 @@ router.get("/mycart/:id/:item_no/:order_id", async function (req, res, next) {
 })
 
 // payment
-router.get("/payment/", async function (req, res, next) {
+router.get("/payment/:id/:order_id", async function (req, res, next) {
+  const conn = await pool.getConnection()
+  await conn.beginTransaction()
   try {
-    // const [rows1, fields1] = await pool.query("")
-    res.render("user/payment")
+    const [rows1, fields1] = await conn.query("SELECT * FROM `order` WHERE order_id=?", [req.params.order_id])
+    const [rows2, fields2] = await conn.query("SELECT * FROM `user` WHERE user_id=?", [req.params.id])
+    await conn.commit()
+    res.render("user/payment", { carts: JSON.stringify(rows1), users: JSON.stringify(rows2) })
   } catch (err) {
-    return next(err)
+    await conn.rollback()
+    next(err)
+  } finally {
+    console.log("finally")
+    await conn.release()
+  }
+})
+
+router.post("/payment/:id/:order_id", upload.single("slip"), async function (req, res, next) {
+  const conn = await pool.getConnection()
+  await conn.beginTransaction()
+
+  const account = req.body.account
+
+  const file = req.file
+  if (!file) {
+    const error = new Error("Please upload a file")
+    error.httpStatusCode = 400
+    return next(error)
+  }
+
+  try {
+    const [rows1, fields1] = await conn.query("INSERT INTO `payment` (payment_slip, payment_account, order_id) VALUES(?, ?, ?)", [
+      file.path.substring(7),
+      account,
+      req.params.order_id,
+    ])
+    console.log(rows1.insertId)
+    const [rows2, fields2] = await conn.query("SELECT * FROM payment JOIN `order_item` USING(order_id) WHERE payment_id=?", [rows1.insertId])
+
+    let order_id_payment = rows2[0].order_id
+    let order_id_order = req.params.order_id
+
+    if (order_id_payment == order_id_order) {
+      const [rows3, fields3] = await conn.query("UPDATE `order` SET order_status=? WHERE order_id=?", ["complete", order_id_payment])
+      const [rows1, fields1] = await conn.query("DELETE FROM `order_item` WHERE order_id=?", [order_id_payment])
+    }
+    await conn.commit()
+    // res.render("own-course", { data: JSON.stringify(rows4), users: JSON.stringify(rows5) })
+    res.redirect("/allcourse/" + req.params.id + "/mycourse")
+  } catch (err) {
+    await conn.rollback()
+    next(err)
+  } finally {
+    console.log("finally")
+    await conn.release()
   }
 })
 
@@ -342,8 +397,8 @@ router.get("/allcourse/:id/mycourse", async function (req, res, next) {
   try {
     const [rows, fields] = await conn.query("SELECT * FROM user WHERE user_id=? ", [req.params.id])
     const [rows1, fields1] = await conn.query(
-      "SELECT * FROM order_item join course using(course_id) join `order` using(order_id) join user using(user_id) join course_image using(course_id) WHERE user_id=?",
-      [req.params.id]
+      "SELECT * FROM my_course join course using(course_id) join `order` using(order_id) join user using(user_id) join course_image using(course_id) WHERE user_id=? AND order_status=?",
+      [req.params.id, "complete"]
     )
     return res.render("own-course", { data: JSON.stringify(rows), users: JSON.stringify(rows1) })
   } catch (err) {
