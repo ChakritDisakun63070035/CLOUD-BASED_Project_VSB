b29385ab07205644569be1c286d86d80acd29862
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2023-02-11 21:27:22 +0700

    update

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '931755',
-  database: 'web4',
+  password: 'your password mysql',
+  database: 'db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
