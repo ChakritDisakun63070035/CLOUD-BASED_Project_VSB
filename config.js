7045d93520262c2d2da18c436be327318b60ce8a
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2023-04-30 17:24:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '123456',
-  database: 'VSB3',
+  password: '087493765ผรื',
+  database: 'db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
